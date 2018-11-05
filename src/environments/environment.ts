4fb38e5d44114f808b7da9ef583399b6f9af07dc
Alejandro Mateus <alejo.mateus.ud@gmail.com>
2018-11-04 20:33:00 -0500

    Config Firebase and read Collections from Firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+	production: false,
+	firebase: {
+		apiKey: "AIzaSyBFw5jNP-JF7ZVR_pb5l3ATbqx5jrstSt4",
+		authDomain: "taskangular.firebaseapp.com",
+		databaseURL: "https://taskangular.firebaseio.com",
+		projectId: "taskangular",
+		storageBucket: "taskangular.appspot.com",
+		messagingSenderId: "858517657089"
+	}
+};
